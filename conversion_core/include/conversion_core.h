b65174c9583560285d693f4982bc0b0b6a5f88f3
softwareguycoder <softwareguycoder@gmail.com>
2019-04-10 08:44:17 -0400

    Introducing Hungarian notation

--- conversion_core/include/conversion_core.h
+++ conversion_core/include/conversion_core.h
@@ -10,6 +10,6 @@
 
 #include "symbols.h"
 
-int char_to_long(const char* input, long* output);
+int StringToLong(const char* input, long* output);
 
 #endif /* __CONVERSION_CORE_H__ */
--- conversion_core/src/conversion_core.c
+++ conversion_core/src/conversion_core.c
@@ -8,35 +8,35 @@
 #include "stdafx.h"
 #include "conversion_core.h"
 
-int char_to_long(const char* input, long* output) {
-	char *endptr = NULL; /* pointer to addtional chars */
-	int base = 10; /* default to reading digits and interpreting as base 10 */
-	long number = 0; /* variable holding return */
+int StringToLong(const char* pszInput, long* plOutput) {
+	char	*pszEndingPtr = NULL; /* pointer to addtional chars */
+	int 	nBase = 10; /* default to reading digits and interpreting as base 10 */
+	long 	lResult = 0; /* variable holding return */
 
 	/* reset errno to zero before call */
 	errno = 0;
 
 	/* call to strtol assigning return to number */
-	number = strtol(input, &endptr, base);
+	lResult = strtol(pszInput, &pszEndingPtr, nBase);
 
 	/* test return to number and errno values */
-	if (input == endptr)
+	if (pszInput == pszEndingPtr)
 		return INVALID_CHARS;
-	else if (errno == ERANGE && number == LONG_MIN)
+	else if (errno == ERANGE && lResult == LONG_MIN)
 		return BUFFER_UNDERFLOW;
-	else if (errno == ERANGE && number == LONG_MAX)
+	else if (errno == ERANGE && lResult == LONG_MAX)
 		return BUFFER_OVERFLOW;
 	else if (errno == EINVAL) /* not all c99 implementations; gcc ok */
 		return INVALID_BASE;
-	else if (errno != 0 && number == 0)
+	else if (errno != 0 && lResult == 0)
 		return UNSPECIFIED_ERROR;
-	else if (errno == 0 && input && !*endptr) {
+	else if (errno == 0 && pszInput && !*pszEndingPtr) {
 		/* result is valid and represents all characters read */
-		*output = number;
+		*plOutput = lResult;
 		return EXACTLY_CORRECT;
-	} else if (errno == 0 && input && *endptr != 0) {
+	} else if (errno == 0 && pszInput && *pszEndingPtr != 0) {
 		/* result is valid but the string contained non-digit characters too */
-		*output = number;
+		*plOutput = lResult;
 		return OK;
 	}
 
